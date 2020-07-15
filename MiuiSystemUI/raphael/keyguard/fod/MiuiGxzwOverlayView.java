1e1acd884a467bcac4bc29cf6fdf9c9faccd0d03
mooseIre <viiplycn@gmail.com>
2020-07-15 22:55:38 +0800

    raphael  v20.7.15

--- MiuiSystemUI/raphael/keyguard/fod/MiuiGxzwOverlayView.java
+++ MiuiSystemUI/raphael/keyguard/fod/MiuiGxzwOverlayView.java
@@ -324,7 +324,7 @@ class MiuiGxzwOverlayView extends GxzwWindowFrameLayout implements MiuiGxzwIconV
     }
 
     private String getBrightnessFile() {
-        String[] stringArray = getResources().getStringArray(285343772);
+        String[] stringArray = getResources().getStringArray(285343774);
         for (int i = 0; i < stringArray.length; i++) {
             if (new File(stringArray[i]).exists()) {
                 return stringArray[i];
