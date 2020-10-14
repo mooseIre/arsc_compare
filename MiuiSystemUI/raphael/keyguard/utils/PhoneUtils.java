0d12b390bcd560c5262c351c479ad37f7bec9595
mooseIre <viiplycn@gmail.com>
2020-10-14 19:05:32 +0800

    raphael  v20.10.14

--- MiuiSystemUI/raphael/keyguard/utils/PhoneUtils.java
+++ MiuiSystemUI/raphael/keyguard/utils/PhoneUtils.java
@@ -1,11 +1,17 @@
 package com.android.keyguard.utils;
 
+import android.app.ActivityOptions;
 import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
 import android.telecom.TelecomManager;
+import com.android.keyguard.EmergencyButton;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import miui.telephony.TelephonyManager;
 
 public class PhoneUtils {
+    private static final Intent INTENT_EMERGENCY_DIAL = new Intent().setAction("com.android.phone.EmergencyDialer.DIAL").setPackage("com.android.phone").setFlags(343932928);
+
     public static void resumeCall(Context context) {
         getTelecommManager(context).showInCallScreen(false);
     }
@@ -25,5 +31,18 @@ public class PhoneUtils {
 
     private static TelecomManager getTelecommManager(Context context) {
         return (TelecomManager) context.getSystemService("telecom");
+    }
+
+    public static void takeEmergencyCallAction(Context context, EmergencyButton.EmergencyButtonCallback emergencyButtonCallback) {
+        if (isInCall(context)) {
+            resumeCall(context);
+            if (emergencyButtonCallback != null) {
+                emergencyButtonCallback.onEmergencyButtonClickedWhenInCall();
+                return;
+            }
+            return;
+        }
+        KeyguardUpdateMonitor.getInstance(context).reportEmergencyCallAction(true);
+        context.startActivityAsUser(INTENT_EMERGENCY_DIAL, ActivityOptions.makeCustomAnimation(context, 0, 0).toBundle(), new UserHandle(KeyguardUpdateMonitor.getCurrentUser()));
     }
 }
