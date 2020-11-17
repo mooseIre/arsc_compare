b70282bf9f2a00a7e8750240b462f3b5ec340f7d
mooseIre <viiplycn@gmail.com>
2020-11-17 17:40:50 +0800

    umi  v20.11.16

--- MiuiSystemUI/umi/keyguard/CarrierText.java
+++ MiuiSystemUI/umi/keyguard/CarrierText.java
@@ -84,7 +84,7 @@ public class CarrierText extends TextView implements NetworkController.CarrierNa
             if (isCustomizationTest()) {
                 ((NetworkController) Dependency.get(cls)).addMobileTypeListener(this);
             }
-            ((NetworkController) Dependency.get(cls)).addCallback((NetworkController.SignalCallback) this);
+            ((NetworkController) Dependency.get(cls)).addCallback(this);
             return;
         }
         this.mSupportNetwork = false;
@@ -175,7 +175,7 @@ public class CarrierText extends TextView implements NetworkController.CarrierNa
             if (isCustomizationTest()) {
                 ((NetworkController) Dependency.get(cls)).removeMobileTypeListener(this);
             }
-            ((NetworkController) Dependency.get(cls)).removeCallback((NetworkController.SignalCallback) this);
+            ((NetworkController) Dependency.get(cls)).removeCallback(this);
         }
         ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).removeDarkReceiver((DarkIconDispatcher.DarkReceiver) this);
     }
