e624f26240c91d21e5cf7f514c90242db7e5c9fe
mooseIre <viiplycn@gmail.com>
2021-06-17 19:08:19 +0800

    umi  v21.6.16

--- MiuiSystemUI/umi/keyguard/injector/KeyguardSensorInjector$unregisterPickupSensor$1.java
+++ MiuiSystemUI/umi/keyguard/injector/KeyguardSensorInjector$unregisterPickupSensor$1.java
@@ -3,9 +3,8 @@ package com.android.keyguard.injector;
 import android.hardware.SensorManager;
 import android.util.Slog;
 
-/* access modifiers changed from: package-private */
 /* compiled from: KeyguardSensorInjector.kt */
-public final class KeyguardSensorInjector$unregisterPickupSensor$1 implements Runnable {
+final class KeyguardSensorInjector$unregisterPickupSensor$1 implements Runnable {
     final /* synthetic */ KeyguardSensorInjector this$0;
 
     KeyguardSensorInjector$unregisterPickupSensor$1(KeyguardSensorInjector keyguardSensorInjector) {
